c6718c24f10dbdde6b12aebfb4d4ccf656246ad3
vprats <vprats@grupo-pinero.com>
2017-07-18 13:10:11 +0200

    * Arreglo por el cambio en la visualización de growls

--- resources/data/mensajes.ts
+++ resources/data/mensajes.ts
@@ -13,26 +13,32 @@ export class Mensajes {
     }
 
     showInfoMessage(alert: string) {
+        this.msgs = []
         this.msgs.push({severity: 'info', summary: 'Info', detail: alert});
     }
 
     showWarnMessage(alert: string) {
+        this.msgs = []
         this.msgs.push({severity: 'warn', summary: 'Atención', detail: alert});
     }
 
     showErrorMessage(alert: string) {
+        this.msgs = []
         this.msgs.push({severity: 'error', summary: 'Error', detail: alert});
     }
 
     showInfoAlert(alert: string) {
+        this.alerts = []
         this.alerts.push({severity: 'info', summary: 'Info', detail: alert});
     }
 
     showWarnAlert(alert: string) {
+        this.alerts = []
         this.alerts.push({severity: 'warn', summary: 'Atención', detail: alert});
     }
 
     showErrorAlert(alert: string) {
+        this.alerts = []
         this.alerts.push({severity: 'error', summary: 'Error', detail: alert});
     }
 
