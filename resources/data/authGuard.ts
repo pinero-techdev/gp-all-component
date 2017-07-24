25d57b1e7f7a9b2a20c0c2fa217ff73deee07d9f
vprats <vprats@grupo-pinero.com>
2017-07-24 10:33:40 +0200

    * Añadido seguridad en navegación dentro del menu. Ahora solo puedes ir a las opciones que el usuario tiene activas

--- resources/data/authGuard.ts
+++ resources/data/authGuard.ts
@@ -1,26 +1,54 @@
 import {Injectable} from "@angular/core";
 import {Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from "@angular/router";
 import {GlobalService} from "../../services/global.service";
+import {MenuRq} from "./menuRq";
+import {AppMenuService} from "../../services/app-menu.service";
 import {AppMenuProviderService} from "../../services/app-menu-provider.service";
+import {Observable} from "rxjs/Rx";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
     constructor(private _router: Router,
-                private _globalService: GlobalService, private _appMenuProviderService: AppMenuProviderService) {
+                private _globalService: GlobalService,
+                private _menu: AppMenuService,
+                private _menuAppMenuProviderService: AppMenuProviderService) {
     }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+
+        let userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
+        let userId = null;
+        if ( userInfo != undefined && userInfo != null ){
+            userId = userInfo.userId;
+        }
+
         let url: string = state.url;
         console.log(url);
         console.log(route.pathFromRoot);
         console.log("Guard, canActivate, globalService: " + this._globalService.globalStatus() + " " + sessionStorage.getItem('userInfo'));
         if ((this._globalService.logged || null != sessionStorage.getItem('userInfo'))) {
-            return true;
+            // 'home' is the default page when user is logged
+            if (url == '/home' || url == '/') {
+                return Observable.of(true);
+            } else {
+                let request: MenuRq = new MenuRq(userId, GlobalService.APP);
+                return this._menu.obtenMenu(request)
+                    .map(
+                        menu => {
+
+                            if ( menu ) {
+                                // Check if option menu is active
+                                return this._menuAppMenuProviderService.isOpcionMenuActivo(menu, url);
+                            }
+                        }
+                    );
+            }
+
         } else {
             // not logged in so redirect to login page
             this._router.navigate(['/login']);
-            return false;
+            return Observable.of(false);
         }
     }
 }
--- services/app-menu-provider.service.ts
+++ services/app-menu-provider.service.ts
@@ -8,5 +8,18 @@ export class AppMenuProviderService {
 
     getEstructuraMenu() : any[] { return null; }
     obtenOpcionesActivas(rq: MenuRq) : Observable<any> { return null; }
+    isOpcionMenuActivo(menu: any[], accion: string): boolean {
+        for( let opcMenu of menu) {
+            if (opcMenu.action && opcMenu.action == accion) {
+                return opcMenu.enabled;
+            } else if ( opcMenu.submenus) {
+                let opcion = this.isOpcionMenuActivo( opcMenu.submenus, accion );
+                if (opcion) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 }
