a0274cf84252ae6e29524d3bd024a16174e0811b
bdomenge <bdomenge@grupo-pinero.com>
2018-05-25 15:18:22 +0200

    * Opciones de configuracion extra para Loading indicator

--- components/loading-indicator/gp.app.loading-indicator.component.ts
+++ components/loading-indicator/gp.app.loading-indicator.component.ts
@@ -1,7 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'gp-app-loading-indicator',
-  templateUrl: './gp.app.loading-indicator.component.html'
+    selector: 'gp-app-loading-indicator',
+    templateUrl: './gp.app.loading-indicator.component.html'
 })
-export class GpAppLoadingIndicatorComponent {}
+export class GpAppLoadingIndicatorComponent {
+    @Input() msg:string = "Recuperando datos ...";
+    @Input() w:string = "40px";
+    @Input() h:string = "40px";
+}
