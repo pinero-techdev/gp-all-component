78a2a2a3c3f75d6bc1503829b4cc27ef9f2b2bf3
vprats@gpi.local <vprats@grupo-pinero.com>
2018-07-04 09:48:39 +0200

    * Arreglo auto-login

--- resources/data/loginRq.ts
+++ resources/data/loginRq.ts
@@ -3,13 +3,14 @@ export class LoginRq {
     password: string;
 	aplicacion: string;
 	params: LoginRqParam[];
+	otherparams: string;
 	
     /**
      * Constructor de la petición de login
      * @param usuario
      * @param password
      */
-    constructor(usuario: string, password: string, aplicacion?: string, params?: LoginRqParam[] ) {
+    constructor(usuario: string, password: string, aplicacion?: string, params?: LoginRqParam[], otherparams?: string ) {
         this.usuario = usuario;
         this.password = password;
 		if( aplicacion )
@@ -19,6 +20,10 @@ export class LoginRq {
 		if( params )
 		{
 			this.params = params;
+		}
+		if ( otherparams )
+		{
+			this.otherparams = otherparams;
 		}
     }
 }
