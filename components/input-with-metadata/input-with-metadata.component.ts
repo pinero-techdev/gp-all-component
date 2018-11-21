94c7a25de6024d7daa464a48fe15ab298f372625
Pau Gallardo <pau.gallardo@in2.es>
2018-11-21 08:15:56 +0100

    ADD column-metadata input to input-with-metadata

--- components/input-with-metadata/input-with-metadata.component.ts
+++ components/input-with-metadata/input-with-metadata.component.ts
@@ -1,6 +1,7 @@
-import {Component, ViewEncapsulation} from "@angular/core";
+import {Component, Input, ViewEncapsulation} from "@angular/core";
 import {NG_VALUE_ACCESSOR} from "@angular/forms";
 import {CustomInput} from "../../resources/data/custom-input";
+import {TableColumnMetadata} from "../../resources/data/table-column-metadata.model";
 
 @Component({
     selector: 'gp-app-input-with-metadata',
@@ -11,5 +12,5 @@ import {CustomInput} from "../../resources/data/custom-input";
     ]
 })
 export class GpAppInputWithMetadataComponent extends CustomInput {
-    
+    @Input() column: TableColumnMetadata;
 }
