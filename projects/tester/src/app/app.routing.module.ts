6181d73c2c6c08accbce54262878274248f2822e
Estefania <estefania.ramirez@todopatterson.com>
2019-04-15 10:21:31 +0200

    AppTesterModule: Add path foo

--- projects/tester/src/app/app.routing.module.ts
+++ projects/tester/src/app/app.routing.module.ts
@@ -1,7 +1,13 @@
+import { FooTesterComponent } from './foo-tester/foo-tester.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-export const appRoutes: Routes = [];
+export const appRoutes: Routes = [
+    {
+        path: 'foo',
+        component: FooTesterComponent,
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(appRoutes)],
