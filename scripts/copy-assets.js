acd6e1536109c589e904b5749d14baf84d285c1b
Estefania <estefania.ramirez@todopatterson.com>
2019-05-10 13:09:33 +0200

    GPCTS-40: Add primeng

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -2,35 +2,34 @@
 const cpx = require('cpx');
 const fs = require('fs');
 
-// Entry dir
-const images = './projects/gp-all-component/src/lib/resources/images/**/*.*';
-const fonts = './projects/gp-all-component/src/lib/resources/fonts/**/*.*';
-const webfonts = './projects/gp-all-component/src/lib/resources/webfonts/**/*.*';
-
-// Destination dir
-const destImages = './dist/gp-all-component/lib/assets/images';
-const destFonts = './dist/gp-all-component/lib/assets/fonts';
-const destWebfonts = './dist/gp-all-component/lib/assets/webfonts';
-
-if (!fs.existsSync(destImages)) {
-  fs.mkdirSync(destImages, { recursive: true });
+function init() {
+  // Get the params by command
+  const args = process.argv.slice(2);
+  let entry, dest;
+  if (args && args.length === 2) {
+    entry = args[0];
+    dest = args[1];
+  }
+  copy(entry, dest);
 }
 
-if (!fs.existsSync(destFonts)) {
-  fs.mkdirSync(destFonts, { recursive: true });
+/* Copy files to destination folder */
+function copy(entry, dest) {
+  if (entry && dest) {
+    if (!fs.existsSync(dest)) {
+      fs.mkdirSync(dest, { recursive: true });
+    }
+
+    /**
+     * https://github.com/mysticatea/cpx
+     */
+    cpx.copy(entry, dest, callback);
+  }
 }
 
-if (!fs.existsSync(destWebfonts)) {
-  fs.mkdirSync(destWebfonts, { recursive: true });
-}
-
+/** Callback after copy files */
 function callback() {
   console.log('[INFO] Copy files finished succesfully!');
 }
 
-/**
- * https://github.com/mysticatea/cpx
- */
-cpx.copy(images, destImages, callback);
-cpx.copy(fonts, destFonts, callback);
-cpx.copy(webfonts, destWebfonts, callback);
+init();
