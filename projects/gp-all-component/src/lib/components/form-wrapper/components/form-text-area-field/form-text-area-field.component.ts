50368329229166eb328696f3ccd8f4d90c20b0a5
Estefania <estefania.ramirez@todopatterson.com>
2019-05-14 14:18:05 +0200

    GPCTS-33: create a variable for keyfields

--- projects/gp-all-component/src/lib/components/form-wrapper/components/form-text-area-field/form-text-area-field.component.ts
+++ projects/gp-all-component/src/lib/components/form-wrapper/components/form-text-area-field/form-text-area-field.component.ts
@@ -97,17 +97,20 @@ export class FormTextAreaFieldComponent extends GpFormFieldControl implements On
    */
   copyValueFromEditedRowToControl(editedRow: any = null) {
     const metadata = this.getFieldMetadata();
-    const keyFields = this.displayInfo.translationInfo.keyFields;
 
     if (metadata && editedRow) {
+      const keyFields =
+        this.displayInfo && this.displayInfo.translationInfo
+          ? this.displayInfo.translationInfo.keyFields
+          : [];
+
       this.currentValue = editedRow[metadata.fieldName];
+
       /* If it has translation, we collect all the values of the fields that act as
       identifiers and put them together to create the translation table identifier */
-      if (this.displayInfo.translationInfo && keyFields) {
-        this.translationKeys = '';
-        for (const keyField of keyFields) {
-          this.translationKeys += editedRow[keyField];
-        }
+      this.translationKeys = '';
+      for (const keyField of keyFields) {
+        this.translationKeys += editedRow[keyField];
       }
     }
   }
