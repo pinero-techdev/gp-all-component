175416abcc850932263135a34646bfdc2ee76748
Sergio Márquez <sergio.marquez@patterson.agency>
2019-05-15 13:11:48 +0200

    [GPCTS-24] Refactor and tests for calendar

--- projects/gp-all-component/src/lib/components/form-wrapper/components/form-calendar-field/form-calendar-field.component.spec.ts
+++ projects/gp-all-component/src/lib/components/form-wrapper/components/form-calendar-field/form-calendar-field.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { LocaleES } from '@lib/resources/localization/es-ES.lang';
 import { FormCalendarFieldComponent } from './form-calendar-field.component';
 import { CommonModule } from '@angular/common';
 import { FormWrapperSharedModules } from '../../../../shared/imports/form-wrapper-shared';
@@ -27,5 +28,57 @@ describe('FormCalendarFieldComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should return fieldMetadata', () => {
+    const metadata = component.getFieldMetadata();
+    expect(metadata).toEqual(component.formField.fieldMetadata);
+  });
+
+  it('should copy value from edited row to control and get it', () => {
+    const editedFormField = {
+      ...FormFieldMock,
+      cansCodi: '2015-03-22',
+      fieldMetadata: { ...FormFieldMock.fieldMetadata, notNull: true },
+    };
+
+    component.copyValueFromEditedRowToControl(editedFormField);
+    component.copyValueFromControlToEditedRow(editedFormField);
+
+    expect(editedFormField[component.formField.fieldMetadata.fieldName]).toBeTruthy();
+  });
+
+  describe('on validate field', () => {
+    describe('validating not null condition', () => {
+      it('should pass', () => {
+        const editedFormField = {
+          ...FormFieldMock,
+          cansCodi: '2015-03-22',
+          fieldMetadata: { ...FormFieldMock.fieldMetadata, notNull: true },
+        };
+
+        const valid = component.validateField(editedFormField);
+
+        expect(valid).toBeTruthy();
+        expect(component.formField.validField).toBeTruthy();
+      });
+
+      it('should not pass', () => {
+        const editedFormField = {
+          ...FormFieldMock,
+          cansCodi: '',
+          fieldMetadata: { ...FormFieldMock.fieldMetadata, notNull: true },
+        };
+
+        const valid = component.validateField(editedFormField);
+
+        expect(valid).toBeFalsy();
+        expect(component.formField.validField).toBeFalsy();
+        expect(component.formField.fieldMsgs).toContain({
+          severity: 'error',
+          detail: LocaleES.VALUE_IS_REQUIRED,
+        });
+      });
+    });
   });
 });
--- projects/gp-all-component/src/lib/components/form-wrapper/components/form-calendar-field/form-calendar-field.component.ts
+++ projects/gp-all-component/src/lib/components/form-wrapper/components/form-calendar-field/form-calendar-field.component.ts
@@ -1,95 +1,82 @@
 import { Component, Input } from '@angular/core';
+import { LocaleES } from '@lib/resources/localization/es-ES.lang';
 import { GpFormFieldControl } from '../../resources/form-field-control.class';
 import { GpFormField } from '../../resources/form-field.model';
 import { DataTableMetaDataField } from '../../../../resources/data/data-table/meta-data/data-table-meta-data-field.model';
 import { GPUtil } from '../../../../services/core/gp-util.service';
+import { CalendarLocaleConfigES } from '../../../../resources/data/calendar/calendar-locale.config';
 
 @Component({
   selector: 'gp-form-calendar-field',
   templateUrl: './form-calendar-field.component.html',
-  styleUrls: ['./form-calendar-field.component.scss'],
 })
 export class FormCalendarFieldComponent extends GpFormFieldControl {
+  /**
+   * The formField for this component
+   */
   @Input() formField: GpFormField;
 
-  dateFormat = 'dd/mm/yy';
+  /**
+   * The locale configuration for component
+   */
+  config = CalendarLocaleConfigES;
 
-  es = {
-    closeText: 'Cerrar',
-    prevText: '<Ant',
-    nextText: 'Sig>',
-    currentText: 'Hoy',
-    monthNames: [
-      'enero',
-      'febrero',
-      'marzo',
-      'abril',
-      'mayo',
-      'junio',
-      'julio',
-      'agosto',
-      'septiembre',
-      'octubre',
-      'noviembre',
-      'diciembre',
-    ],
-    monthNamesShort: [
-      'ene',
-      'feb',
-      'mar',
-      'abr',
-      'may',
-      'jun',
-      'jul',
-      'ago',
-      'sep',
-      'oct',
-      'nov',
-      'dic',
-    ],
-    dayNames: ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado'],
-    dayNamesShort: ['dom', 'lun', 'mar', 'mié', 'jue', 'vie', 'sáb'],
-    dayNamesMin: ['D', 'L', 'M', 'X', 'J', 'V', 'S'],
-    weekHeader: 'Sm',
-    // defaultDate: new Date(),
-    firstDay: 1,
-    isRTL: false,
-    showMonthAfterYear: false,
-    yearSuffix: '',
-    firstDayOfWeek: 1,
-    today: 'Today',
-    clear: 'Clear',
-  };
+  /**
+   * Date default format
+   */
+  dateFormat = 'dd/mm/yy';
 
   constructor() {
     super();
   }
 
+  /**
+   * Returns current field metadata
+   */
   getFieldMetadata(): DataTableMetaDataField {
     return this.formField.fieldMetadata;
   }
 
+  /**
+   * Returns current form field
+   */
   getFormField(): GpFormField {
     return this.formField;
   }
 
+  /**
+   * Copies value from control to editing row
+   * @param editedRow The editing row
+   */
   copyValueFromControlToEditedRow(editedRow: any) {
     editedRow[this.formField.fieldMetadata.fieldName] = GPUtil.dateToYyyymmdd(this.currentValue);
   }
 
+  /**
+   * Copies values from editing row to control
+   * @param editedRow The editing row
+   */
   copyValueFromEditedRowToControl(editedRow: any) {
     const value = editedRow[this.formField.fieldMetadata.fieldName];
     this.currentValue = GPUtil.str2Date(value, 'YYYY-MM-DD');
   }
 
+  /**
+   * Starts validation for editing row
+   * @param editedRow The editing row
+   */
   validateField(editedRow: any) {
     this.formField.validField = true;
     this.formField.fieldMsgs = null;
     const inputValue = editedRow[this.formField.fieldMetadata.fieldName];
 
-    if (this.formField.fieldMetadata.notNull && (inputValue === '' || inputValue === null)) {
+    // a) Check nullability
+    const notNullable =
+      this.formField.fieldMetadata.notNull && (inputValue === '' || inputValue === null);
+
+    if (notNullable) {
       this.formField.validField = false;
-      this.validateFieldAddMsgs('El valor es obligatorio.');
+      this.validateFieldAddMsgs(LocaleES.VALUE_IS_REQUIRED);
       return false;
     }
 
