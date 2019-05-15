93a43560edd047fd5bfcaa81d9f8aaf502761a7d
Sergio Márquez <sergio.marquez@patterson.agency>
2019-05-15 10:13:44 +0200

    [GPCTS-15] Included redirect tester

---
+++ projects/tester/src/app/shared/redirect-tester/redirect-tester.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-redirect-tester',
+  templateUrl: './redirect-tester.component.html',
+})
+export class RedirectTesterComponent {}
--- projects/tester/src/app/shared/shared.routing.module.ts
+++ projects/tester/src/app/shared/shared.routing.module.ts
@@ -13,6 +13,7 @@ import { TopbarTesterComponent } from './topbar-tester/topbar-tester.component';
 import { TableCrudTesterComponent } from './table-crud-tester/table-crud-tester.component';
 import { DropdownTesterComponent } from './dropdown-tester/dropdown-tester.component';
 import { ImgTesterComponent } from './img-tester/img-tester.component';
+import { RedirectComponent } from '@lib/components/redirect/redirect.component';
 
 export const appRoutes: Routes = [
   {
@@ -66,6 +67,10 @@ export const appRoutes: Routes = [
   {
     path: 'img-tester',
     component: ImgTesterComponent,
+  },
+  {
+    path: 'redirect-tester/:new/:url',
+    component: RedirectComponent,
   },
 ];
 
