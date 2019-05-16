86e0f61c20b3c74cd972c1701351d83cb7dc8f11
Rafa Miranda <rmiranda@todopatterson.com>
2019-05-16 10:02:43 +0200

    fix(GPCTS-37): fixing typescript wrong inputs deletion

--- projects/gp-all-component/src/lib/components/rating/rating.component.ts
+++ projects/gp-all-component/src/lib/components/rating/rating.component.ts
@@ -1,12 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gp-rating',
   templateUrl: './rating.component.html',
   styleUrls: ['./rating.component.scss'],
 })
-export class RatingComponent {
-  public starsArray = [1, 2, 3, 4, 5];
+export class RatingComponent implements OnInit {
+  starsArray = [1, 2, 3, 4, 5];
 
-  @Input() stars;
+  @Input() value: number;
+  @Input() iconOn = 'pi-star';
+  @Input() iconOff = 'pi-star';
+  @Input() styleClassOn = 'gold';
+  @Input() styleClassOff = '';
+  @Input() styleOn = '';
+  @Input() styleOff = '';
+  @Input() stars: number;
+
+  ngOnInit(): void {
+    this.value = this.stars;
+  }
+
+  getRatingClass(i): string {
+    return this.value > i
+      ? `${this.iconOn} ${this.styleClassOn}`
+      : `${this.iconOff} ${this.styleClassOff}`;
+  }
 }
