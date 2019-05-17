6d12cfaf96a46e85d4744cb97e139e13453e202b
Sergio Márquez <sergio.marquez@patterson.agency>
2019-05-17 07:57:14 +0200

    [GPCTS-25] Refactor checkbox field and docs

--- projects/gp-all-component/src/lib/components/form-wrapper/components/form-checkbox-field/form-checkbox-field.component.ts
+++ projects/gp-all-component/src/lib/components/form-wrapper/components/form-checkbox-field/form-checkbox-field.component.ts
@@ -2,13 +2,17 @@ import { Component, Input } from '@angular/core';
 import { GpFormFieldControl } from '../../resources/form-field-control.class';
 import { GpFormField } from '../../resources/form-field.model';
 import { DataTableMetaDataField } from './../../../../resources/data/data-table/meta-data/data-table-meta-data-field.model';
+import { FormGenericField } from '../../resources/form-generic-field.interface';
 
 @Component({
   selector: 'gp-form-checkbox-field',
   templateUrl: './form-checkbox-field.component.html',
   styleUrls: ['./form-checkbox-field.component.scss'],
 })
-export class FormCheckboxFieldComponent extends GpFormFieldControl {
+export class FormCheckboxFieldComponent extends GpFormFieldControl implements FormGenericField {
+  /**
+   * The formField for this component
+   */
   @Input() formField: GpFormField;
 
   constructor() {
@@ -23,19 +27,19 @@ export class FormCheckboxFieldComponent extends GpFormFieldControl {
     return this.formField ? this.formField.fieldMetadata : null;
   }
 
-  copyValueFromControlToEditedRow(editedRow: any) {
+  copyValueFromControlToEditedRow(editedRow: any): void {
     editedRow[this.formField.fieldMetadata.fieldName] = this.currentValue
       ? this.formField.fieldMetadata.displayInfo.checkedValue
       : this.formField.fieldMetadata.displayInfo.uncheckedValue;
   }
 
-  copyValueFromEditedRowToControl(editedRow: any) {
+  copyValueFromEditedRowToControl(editedRow: any): void {
     this.currentValue =
       this.formField.fieldMetadata.displayInfo.checkedValue ===
       editedRow[this.formField.fieldMetadata.fieldName];
   }
 
-  validateField(editedRow: any = null) {
+  validateField(editedRow: any = null): boolean {
     // A checkbox is always valid.
     if (this.formField !== undefined) {
       this.formField.validField = true;
---
+++ projects/gp-all-component/src/lib/components/form-wrapper/components/form-checkbox-field/form-checkbox-field.md
@@ -0,0 +1,19 @@
+# Form Checkbox Field Component
+
+## Features a wrapper for PrimeNg Checkbox Component
+
+### Example component usage:
+
+```html
+<gp-checkbox-field [formField]="formField">
+</gp-checkbox-field>
+```
+
+### Component Binding Fields
+
+```typescript
+  /**
+   * The formField for this component
+   */
+  @Input() formField: FormField
+```
