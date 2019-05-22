0e539d86cc1a19f7486451dbd9c662e7cde050f8
Estefania <estefania.ramirez@todopatterson.com>
2019-05-22 10:53:57 +0200

    GPCTS-45: Add environment vars

---
+++ projects/gp-all-component/src/lib/components/login/login.routing.module.ts
@@ -0,0 +1,26 @@
+import { LoginComponent } from './login.component';
+import { AuthGuard } from './../../services/core/auth-guard.service';
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+    pathMatch: 'full',
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [AuthGuard],
+    data: { public: true },
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class LoginRoutingModule {}
