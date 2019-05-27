3a828b1be570823f37e6b98efc3371fb8e1c1bb3
Estefania <estefania.ramirez@todopatterson.com>
2019-05-27 09:26:20 +0200

    GPCTS-53: Fix UT

--- projects/gp-all-component/src/lib/components/dynamic/dynamic.component.spec.ts
+++ projects/gp-all-component/src/lib/components/dynamic/dynamic.component.spec.ts
@@ -1,51 +1,52 @@
-import { DynamicComponent, DynamicMetadata } from './dynamic.component';
-import { Component, ComponentFactoryResolver } from '@angular/core';
-import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { DynamicComponent, DynamicMetadata } from './dynamic.component';
+import { Component } from '@angular/core';
+import { TestBed, ComponentFixture, async } from '@angular/core/testing';
 
 @Component({
-    selector: 'gp-test-hosted',
-    template: 'templ',
+  selector: 'gp-test-hosted',
+  template: 'templ',
 })
 class HostedComponent {
-    content: string;
+  content: string;
 }
 
-describe('DynamicComponent', () => {
-    let fixture: ComponentFixture<DynamicComponent>;
-    let component: DynamicComponent;
+fdescribe('DynamicComponent', () => {
+  let fixture: ComponentFixture<DynamicComponent>;
+  let component: DynamicComponent;
 
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            providers: [ComponentFactoryResolver],
-            declarations: [HostedComponent, DynamicComponent],
-        });
-
-        // some sort of hack to get this working
-        // https://github.com/angular/angular/issues/10760
-        TestBed.overrideModule(BrowserDynamicTestingModule, {
-            set: {
-                entryComponents: [HostedComponent],
-            },
-        });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HostedComponent, DynamicComponent],
     });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(DynamicComponent);
-        component = fixture.componentInstance;
+    TestBed.overrideModule(BrowserDynamicTestingModule, {
+      set: {
+        entryComponents: [HostedComponent],
+      },
     });
 
-    it('should create', async(() => {
-        expect(fixture.componentInstance).toBeTruthy();
-    }));
+    TestBed.compileComponents();
+  });
 
-    xit('should instantiatе hosted component correctly', async () => {
-        const data: DynamicMetadata = {
-            component: HostedComponent,
-        };
-        component.componentData = data;
-        // component.ngOnInit();
-        fixture.detectChanges();
-        expect(fixture.componentInstance.dynamicComponent.length).toBe(1);
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DynamicComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', async(() => {
+    expect(fixture.componentInstance).toBeTruthy();
+    expect(component).toBeTruthy();
+  }));
+
+  it('should instantiatе hosted component correctly', async () => {
+    const data: DynamicMetadata = {
+      component: HostedComponent,
+    };
+    component.componentData = data;
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(fixture.componentInstance.dynamicComponent.length).toBe(1);
+    expect(fixture.nativeElement.querySelector('gp-test-hosted')).toBeTruthy();
+  });
 });
