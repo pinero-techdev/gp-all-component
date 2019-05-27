d4f87c45cf7c46a96c5347b350700805028edfc9
Alvaro Ramirez <aramirez@todopatterson.com>
2019-05-27 12:07:32 +0200

    refactor(GPCTS-45): formFieldType property from GpFormField class string to enum type

--- projects/gp-all-component/src/lib/components/form-wrapper/resources/form-field.model.ts
+++ projects/gp-all-component/src/lib/components/form-wrapper/resources/form-field.model.ts
@@ -1,11 +1,12 @@
 import { Message } from 'primeng/components/common/api';
 import { DataTableMetaDataField } from '../../../resources/data/data-table/meta-data/data-table-meta-data-field.model';
 import { GpFormControl } from './form-control.model';
+import { GpFormFieldType } from './form-field-type.enum';
 
 // TODO optimizar 17/12/2018
 export class GpFormField {
   /* Tipo de control usado.*/
-  formFieldType: string = null;
+  formFieldType: GpFormFieldType;
   /* Indica si el campo es valido o no. */
   validField = true;
   /* MensajesComponent de error asociados del campo. */
