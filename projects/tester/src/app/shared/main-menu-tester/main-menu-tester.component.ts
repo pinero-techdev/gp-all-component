902aebb8440964ea3b279572a1b1bc9c373caa0a
Alvaro Ramirez <aramirez@todopatterson.com>
2019-05-29 17:01:34 +0200

    Revert "[GPCTS-78] Added tester project"
    This reverts commit f5e0a209486a370a9675a0dc33960154bc7b056a.

--- projects/tester/src/app/shared/main-menu-tester/main-menu-tester.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import 'rxjs/add/observable/of';
-
-@Component({
-  selector: 'app-main-menu-tester',
-  templateUrl: './main-menu-tester.component.html',
-  styleUrls: ['./main-menu-tester.component.scss'],
-})
-export class MainMenuTesterComponent {}
--- projects/tester/src/app/shared/table-frame-tester/table-frame-tester.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-table-frame-tester',
-  templateUrl: './table-frame-tester.component.html',
-  styleUrls: ['./table-frame-tester.component.scss'],
-})
-export class TableFrameTesterComponent {}
