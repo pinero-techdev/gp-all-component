f86032b82faca98eb1241ccce8c234f7ca1200ed
Sergio Márquez <sergio.marquez@patterson.agency>
2019-05-30 08:36:24 +0200

    [GCPTS-91] Base template integration

--- guidelines/.vuepress/config.js
+++ guidelines/.vuepress/config.js
@@ -1,3 +1,15 @@
 module.exports = {
   title: 'Angular Guidelines',
+  description: 'All the best practices you need to know to work on this project',
+
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Getting Started', link: '/bootstrap' },
+      // { text: 'Vue', link: 'https://vuejs.org/' },
+      // { text: 'Nuxt', link: 'https://nuxtjs.org/' },
+    ],
+    sidebar: [['/', 'Introduction'], '/development'],
+    lastUpdated: 'Last Updated',
+  },
 };
--- guidelines/README.md
+++ guidelines/README.md
@@ -1,1 +1,15 @@
-# Hello VuePress
+---
+home: true
+heroImage: #
+sidebar: auto
+actionText: Get Started →
+actionLink: /bootstrap
+features:
+  - title: Fancy Feature
+    details: Fancy Description
+  - title: Fancy Feature
+    details: Fancy Description
+  - title: Fancy Feature
+    details: Fancy Description
+footer: Copyright © 2019-present Patterson Agency
+---
