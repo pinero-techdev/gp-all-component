3279043de90081fb1bf6b55e223ae07a53552d92
Rafa Miranda <rmiranda@todopatterson.com>
2019-07-16 14:32:47 +0200

    feat(GPCTS-120): comment every ButtonComponent variable and function

--- projects/gp-all-component/src/lib/components/button/button.component.ts
+++ projects/gp-all-component/src/lib/components/button/button.component.ts
@@ -8,30 +8,61 @@ import { MenuItem } from 'primeng/api';
   styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent implements OnInit {
+  /**
+   * Button type, basic by default
+   */
   @Input()
   type: ButtonType = ButtonType.Basic;
 
+  /**
+   * Button severity, primary by default
+   */
   @Input()
   severity: ButtonSeverity = ButtonSeverity.Primary;
 
+  /**
+   * Button label
+   */
   @Input()
   label: string;
 
+  /**
+   * Button icon
+   */
   @Input()
   icon: string;
 
+  /**
+   * Item buttons to show in overlaypanel in case of splitbutton
+   */
   @Input()
   items: MenuItem[];
 
+  /**
+   * Button accesibility. Every button is enabled by default
+   */
   @Input()
   disabled = false;
 
+  /**
+   * Event to emit when a button is clicked
+   */
   @Output()
   onClickEvent = new EventEmitter<boolean>();
 
+  /**
+   * Items to show if button is type splitbutton
+   */
   splitItems: MenuItem[];
+
+  /**
+   * Severity for splitbutton to bind in template
+   */
   splitButtonSeverity: string;
 
+  /**
+   * When component is initialized splitItems array and splitButtonSeverity must be asigned
+   */
   ngOnInit() {
     if (this.type === ButtonType.Split) {
       this.splitItems = [...this.items];
@@ -39,10 +70,16 @@ export class ButtonComponent implements OnInit {
     }
   }
 
+  /**
+   * Returns the click event emit
+   */
   onClick() {
     return this.onClickEvent.emit(true);
   }
 
+  /**
+   * Returns the class for splitbutton severity
+   */
   getSplitButtonSeverity() {
     const severityClassName =
       this.severity === ButtonSeverity.Danger
