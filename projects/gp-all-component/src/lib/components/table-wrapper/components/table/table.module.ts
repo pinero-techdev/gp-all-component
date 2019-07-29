976dfc06a6ce38f7e05b8101f7442080b9e54669
Sergio Márquez <sergio.marquez@patterson.agency>
2019-07-29 08:26:51 +0200

    [GPCTS-122] Created memo pipe

--- projects/gp-all-component/src/lib/components/table-wrapper/components/table/table.module.ts
+++ projects/gp-all-component/src/lib/components/table-wrapper/components/table/table.module.ts
@@ -4,6 +4,7 @@ import { TableComponent } from './table.component';
 import { TableModule as PrimeTableModule } from 'primeng/table';
 import { ColumnTemplateDirective } from './directives/column-template.directive';
 import { IsNilPipe } from './pipes/is-nil.pipe';
+import { MemoPipe } from './pipes/memo.pipe';
 import { RowComponent } from './row/row.component';
 import { EditableColumnTemplateDirective } from './directives/editable-column-template.directive';
 
@@ -12,6 +13,7 @@ import { EditableColumnTemplateDirective } from './directives/editable-column-te
     TableComponent,
     ColumnTemplateDirective,
     IsNilPipe,
+    MemoPipe,
     RowComponent,
     EditableColumnTemplateDirective,
   ],
