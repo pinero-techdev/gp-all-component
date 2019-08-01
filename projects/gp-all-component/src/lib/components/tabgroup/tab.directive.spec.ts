17a67e9778f1bde1c532a25a25ddcd0c03eab0e3
Rafa Miranda <rafa.miranda@patterson.agency>
2019-08-01 15:34:34 +0200

    feat(GPCTS-124): unit test advances for tabview component and tab directive

---
+++ projects/gp-all-component/src/lib/components/tabgroup/tab.directive.spec.ts
@@ -0,0 +1,44 @@
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { Component, ViewChild } from '@angular/core';
+import { FormsModule, NgControl } from '@angular/forms';
+import { TabDirective } from './tab.directive';
+
+@Component({
+  template: `
+    <ng-template gpTab tabTitle="Tab1">
+      <div class="tab-panel">
+        <h3>Tab1 title</h3>
+        <p>Tab1 panel content.</p>
+      </div>
+    </ng-template>
+  `,
+})
+class TestTabComponent {
+  @ViewChild(TabDirective) tab: TabDirective;
+}
+
+xdescribe('TabDirective', () => {
+  let component: TestTabComponent;
+  let fixture: ComponentFixture<TestTabComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [TabDirective, TestTabComponent],
+      imports: [FormsModule],
+      providers: [NgControl],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestTabComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('dgagdfga', () => {
+    expect(component.tab.tabTitle).toEqual('Tab1');
+  });
+});
