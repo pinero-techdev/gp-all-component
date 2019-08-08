29a239e9cb5ebecf0061d789c0f0f08056e43de2
Rafa Miranda <rafa.miranda@patterson.agency>
2019-08-08 16:53:56 +0200

    feat(GPCTS-125): Slider component finished. Remaining unit tests and markdown

--- projects/gp-all-component/src/lib/components/form-wrapper/components/form-slider-field/form-slider-field.component.ts
+++ projects/gp-all-component/src/lib/components/form-wrapper/components/form-slider-field/form-slider-field.component.ts
@@ -1,13 +1,47 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'gp-form-slider-field',
   templateUrl: './form-slider-field.component.html',
   styleUrls: ['./form-slider-field.component.scss'],
 })
-export class FormSliderFieldComponent {
+export class FormSliderFieldComponent implements OnInit {
+  @Input()
+  label = '';
+
+  @Input()
+  showInput = false;
+
   @Input()
   orientation = 'horizontal';
 
-  val: number;
+  @Input()
+  step = 1;
+
+  @Input()
+  rangeValues: string;
+
+  @Input()
+  range: boolean;
+
+  @Input()
+  min = 0;
+
+  @Input()
+  max = 100;
+
+  val: any;
+  showLabel = false;
+
+  ngOnInit() {
+    this.showLabel = this.label === '' ? false : true;
+    if (this.rangeValues) {
+      this.range = true;
+      this.val = this.rangeValues.split(',').map(Number);
+    } else if (this.min) {
+      this.val = this.min;
+    } else {
+      this.val = 0;
+    }
+  }
 }
