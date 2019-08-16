970d3c0d352edc3a7a535c4bd68201973d28924e
Rafa Miranda <rafa.miranda@patterson.agency>
2019-08-16 10:52:03 +0200

    feat(GPCTS-123): initial modal component structure

---
+++ projects/gp-all-component/src/lib/components/modal/modal.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gp-modal',
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+})
+export class ModalComponent {}
