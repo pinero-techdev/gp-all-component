1ca56528bfcdf4df14c488e259e6206f95cc3b99
Rafa Miranda <rafa.miranda@patterson.agency>
2019-08-22 14:30:03 +0200

    feat(GPCTS-142): default emptymessage from gp-table

--- projects/gp-all-component/src/lib/components/table-wrapper/components/table/table.component.ts
+++ projects/gp-all-component/src/lib/components/table-wrapper/components/table/table.component.ts
@@ -20,6 +20,7 @@ import { TableColumn } from './models/table-column.model';
 import { PaginationOptions } from './models/pagination-options.model';
 import { OnChange } from 'property-watch-decorator';
 import { Table } from 'primeng/table';
+import { LocaleES } from './../../../../resources/localization/es-ES.lang';
 
 @Component({
   selector: 'gp-table',
@@ -36,7 +37,7 @@ export class TableComponent implements AfterContentInit {
 
   @Input() selected = [];
 
-  @Input() emptyMessage: string;
+  @Input() emptyMessage = LocaleES.NO_RECORDS_FOUND;
 
   @Input()
   @OnChange<TableModel>('buildModel')
