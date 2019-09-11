91c4fc6f2ed66c3713448e68f82579113d2d31ee
Estefania <estefania.ramirez@todopatterson.com>
2019-09-11 15:11:17 +0200

    TableCRUD: add buttons

--- projects/gp-all-component/src/lib/shared/imports/table-wrapper-shared.ts
+++ projects/gp-all-component/src/lib/shared/imports/table-wrapper-shared.ts
@@ -1,6 +1,5 @@
 import { MultiLanguageModule } from './../../components/multi-language/multi-language.module';
 import { SharedModule } from './../shared.module';
-import { ButtonModule } from 'primeng/button';
 import { FormWrapperModule } from './../../components/form-wrapper/form-wrapper.module';
 import { GPUtil } from './../../services/core/gp-util.service';
 import { LoadingIndicatorModule } from './../../components/loading-indicator/loading-indicator.module';
@@ -13,6 +12,7 @@ import { TableMetadataService } from '../../services/api/table/table-metadata.se
 import { TableModule } from 'primeng/table';
 import { TableService } from './../../services/api/table/table.service';
 import { ToastModule } from 'primeng/toast';
+import { ButtonModule } from '../../components/button/button.module';
 
 export const TableWrapperSharedModules = [
   ButtonModule,
