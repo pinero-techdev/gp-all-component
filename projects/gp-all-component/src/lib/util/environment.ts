5e976f9c50f65e9a77723145dab03f1f472b1985
Estefania <estefania.ramirez@todopatterson.com>
2019-09-17 08:00:12 +0200

    Env: setting in GlobalService

--- projects/gp-all-component/src/lib/util/environment.ts
+++ projects/gp-all-component/src/lib/util/environment.ts
@@ -1,4 +1,13 @@
-export const environmentBase = {
+export interface IEnvironment {
+  production: boolean;
+  appName: string;
+  appTitle: string;
+  baseUrl: string;
+  loginUrl: string;
+  menuUrl?: string;
+}
+
+export const environmentBase: IEnvironment = {
   production: false,
   appName: '',
   appTitle: '',
