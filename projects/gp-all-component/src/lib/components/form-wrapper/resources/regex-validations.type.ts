41ac2534959d9553f4490683e55c36fc39498352
bdomenge <bdomenge@grupo-pinero.com>
2019-10-25 16:56:46 +0200

    * regex fix

--- projects/gp-all-component/src/lib/components/form-wrapper/resources/regex-validations.type.ts
+++ projects/gp-all-component/src/lib/components/form-wrapper/resources/regex-validations.type.ts
@@ -3,7 +3,7 @@ export class RegexValidations {
   static BLANK_SPACE = /\s/;
   static CONTROL_SPACE = /[\u0000-\u0019]/;
   static SPECIAL_CHARACTERS = /[\u0080-\uFFFF]/;
-  static TIME_24_HOURS = /(([0-1][1-9])|(2[0-3])):[0-5][0-9]/;
+  static TIME_24_HOURS = /^(2[0-3]|[01]?[0-9]):([0-5]?[0-9])/;
 
   static hasBlankSpace = (text: string): boolean => RegexValidations.BLANK_SPACE.test(text);
   static hasControlSpace = (text: string): boolean => RegexValidations.CONTROL_SPACE.test(text);
